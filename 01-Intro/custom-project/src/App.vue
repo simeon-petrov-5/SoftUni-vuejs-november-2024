1a4403716fb123b7675d804a505cf7af4d21dc80
Simeon Petrov <petrov.v.simeon@gmail.com>
2024-11-12 21:35:08 +0200

    chore: Lec 1

---
+++ 01-Intro/custom-project/src/App.vue
@@ -0,0 +1,79 @@
+<script>
+const STEP = 10;
+export default {
+  data() {
+    return {
+      numberOfClicks: 10,
+      dynamic: [{
+        button: {
+          id: 99,
+        },
+      }],
+    };
+  },
+  methods: {
+    increment() {
+      this.numberOfClicks += STEP;
+    },
+    decrement() {
+      this.numberOfClicks -= STEP;
+    },
+    changeButtonId() {
+      this.dynamic[0].button.id = Date.now();
+    },
+  },
+};
+</script>
+
+<template>
+  <h1 class="myTitle">
+    Hello world {{ STEP }}
+  </h1>
+
+  <h2
+    class="btnClicks"
+    :class="[
+      {
+        green: numberOfClicks > 20,
+      },
+      `clicks-${numberOfClicks}`,
+    ]"
+  >
+    The button was clicked
+    {{ numberOfClicks }}
+    number of times!
+  </h2>
+
+  <div class="btnWrapper">
+    <button @click="increment">
+      increment
+    </button>
+
+    <button @click="decrement">
+      decrement
+    </button>
+  </div>
+
+  <button :id="dynamic[0].button.id" @click="changeButtonId">
+    Dynamic ID button: {{ dynamic[0].button.id }}
+  </button>
+</template>
+
+<style scoped>
+.myTitle{
+  color: red;
+}
+.btnWrapper{
+  margin-bottom: 2rem;
+}
+
+.btnClicks{
+  color:lightsalmon
+}
+.btnClicks.green{
+  color:green
+}
+.btnGreen{
+color: green;
+}
+</style>
--- Notes.md
+++ Notes.md
@@ -1,1 +1,3 @@
 # Важни линкове и ресурси
+- What is "this"? Why you should avoid arrow functions on Vue methods https://dev.to/js_bits_bill/what-is-this-why-you-should-avoid-arrow-functions-on-vue-methods-a71
+- UI LIBRARY PrimeVue https://primevue.org/
